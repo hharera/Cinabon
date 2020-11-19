570c53f149de7e8dcf661b2ac9a97f75b0a4e115
Harera <hassanstar201118@gmail.com>
2020-11-19 22:16:26 +0200

    Functionality of Edit button in cart fragment

--- app/src/main/java/Controller/CartRecyclerViewAdapter.java
+++ app/src/main/java/Controller/CartRecyclerViewAdapter.java
@@ -1,10 +1,15 @@
 package Controller;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.BitmapFactory;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -20,6 +25,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
+import java.util.Map;
 
 import Model.Item;
 import Model.Product.Product;
@@ -92,6 +98,48 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
                         totalBill += totalPrice;
                     }
                 });
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editClicked(holder, position, price[0]);
+            }
+        });
+    }
+
+    private void editClicked(ViewHolder holder, int position, float price) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        EditText editText = new EditText(context);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        builder.setView(editText);
+        builder.setTitle("Enter Quantity");
+        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int quantity = Integer.parseInt(editText.getText().toString());
+                holder.quantity.setText(String.valueOf(quantity));
+                holder.total_price.setText(String.valueOf(quantity * price) + " EGP");
+                setQuantity(position, quantity);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void setQuantity(int position, int quantity) {
+        fStore.collection("Users")
+                .document(auth.getUid())
+                .collection("Cart")
+                .document(list.get(position).getCategoryName() + list.get(position).getProductId())
+                .update("quantity", quantity);
     }
 
     @Override
@@ -102,8 +150,9 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
         TextView title, total_price, quantity;
-        TextView total_bill, bill_cost_word;
+        TextView total_bill, bill_cost_word, edit, remove;
         RelativeLayout cart_tail;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -118,6 +167,9 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
             title = itemView.findViewById(R.id.title);
             total_price = itemView.findViewById(R.id.total_price);
             quantity = itemView.findViewById(R.id.quantity);
+
+            edit = itemView.findViewById(R.id.edit);
+            remove = itemView.findViewById(R.id.remove);
         }
     }
 
