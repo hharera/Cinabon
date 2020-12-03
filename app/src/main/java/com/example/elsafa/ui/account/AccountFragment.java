1584b38e57adb1aba92165cd770cbc3d666ae4b0
Harera <hassanstar201118@gmail.com>
2020-12-03 22:06:58 +0200

    Account fragment

--- app/src/main/java/com/example/elsafa/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/elsafa/ui/account/AccountFragment.java
@@ -1,18 +1,26 @@
 package com.example.elsafa.ui.account;
 
+import android.app.Application;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import com.example.elsafa.R;
 
 public class AccountFragment extends Fragment {
 
+
+    private RadioButton black_theme;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +30,28 @@ public class AccountFragment extends Fragment {
         setExitTransition(inflater1.inflateTransition(R.transition.fragment_in));
         setEnterTransition(inflater1.inflateTransition(R.transition.fragment_out));
 
+
+        black_theme = root.findViewById(R.id.black_theme);
+
+//        setListeners();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         return root;
     }
+
+    private void setListeners() {
+        black_theme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    getActivity().setTheme(R.style.Theme_AppCompat_DayNight);
+                } else {
+                    getActivity().setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
+                }
+            }
+        });
+    }
+
+
 }
