4f4ca922210be6ef2e209d2fbc2a914cf2778fcb
Harera <hassanstar201118@gmail.com>
2020-12-06 14:29:29 +0200

    add location

---
+++ app/src/main/java/com/example/elsafa/CafeLocation.java
@@ -0,0 +1,36 @@
+package com.example.elsafa;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CafeLocation extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafe_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng lamera = new LatLng(29.06671001543988, 31.108046795506084);
+        mMap.addMarker(new MarkerOptions().position(lamera).title("Lamera"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lamera));
+    }
+}
