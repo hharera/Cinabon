0021d9520872041c5cec9896983824f733b7ba8a
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-09-23 16:37:33 +0200

    Solash Screen update

---
+++ app/src/main/java/com/hassanharera/padna/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.hassanharera.padna
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.Tasks
+import com.hassanharera.padna.common.BaseActivity
+import com.hassanharera.padna.db.remote.repository.AuthManager
+import com.hassanharera.padna.db.remote.repository.UserRepository
+import com.hassanharera.padna.ui.HomeActivity
+import com.hassanharera.padna.ui.internet.NoInternetActivity
+import com.hassanharera.padna.utils.Response
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.*
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainViewModel.checkLogin()
+        setupAnimation()
+        waitDelay()
+    }
+
+    private fun waitDelay() {
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(1500)
+            startActivity(
+                Intent(this@MainActivity, HomeActivity::class.java)
+            )
+        }
+    }
+
+    private fun setupAnimation() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.waiting_page_transition)
+        val appName = findViewById<TextView?>(R.id.app_name)
+        val apple = findViewById<ImageView?>(R.id.apple)
+        apple.startAnimation(animation)
+        appName.startAnimation(animation)
+    }
+
+    private fun showNoInternet() {
+        val intent = Intent(this@MainActivity, NoInternetActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val authManager: AuthManager,
+    private val userRepo: UserRepository
+) : ViewModel() {
+
+    fun signInAnonymously() = liveData {
+        viewModelScope.launch(Dispatchers.IO) {
+            emit(Response.loading(null))
+
+            val task = authManager.loginAnonymously()
+            Tasks.await(task)
+            emit(
+                if (task.isSuccessful)
+                    Response.success(data = task.result)
+                else
+                    Response.error(error = task.exception, data = null)
+            )
+        }
+    }
+
+    fun checkLogin() {
+        if (authManager.getCurrentUser() == null)
+            authManager.loginAnonymously()
+    }
+}
