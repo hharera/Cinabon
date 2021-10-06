27f867b1a4032f7952b09d2683983cea02fdda52
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-06 16:19:15 +0200

    adding search feature

---
+++ user-features/search/src/main/java/com/harera/search/SearchViewModel.kt
@@ -0,0 +1,43 @@
+package com.harera.hyperpanda.ui.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.harera.model.modelget.Product
+import com.harera.repository.repository.SearchRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val searchRepository: SearchRepository
+) : ViewModel() {
+
+    private val _products: MutableLiveData<List<Product>> = MutableLiveData()
+    val products: LiveData<List<Product>> = _products
+
+    private val _loading: MutableLiveData<Boolean> = MutableLiveData()
+    val loading: LiveData<Boolean> = _loading
+
+    private val _exception: MutableLiveData<Exception> = MutableLiveData()
+    val exception: LiveData<Exception> = _exception
+
+    fun searchProducts(text: String) {
+        _loading.value = true
+        searchRepository
+            .searchProducts(text = text)
+            .addOnSuccessListener {
+                it.documents.map {
+                    it.toObject(Product::class.java)!!
+                }.let {
+                    _products.value = it
+                }
+
+                _loading.value = false
+            }
+            .addOnFailureListener {
+                _exception.value = it
+                _loading.value = false
+            }
+    }
+}
