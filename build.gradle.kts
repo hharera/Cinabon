282d884f5a0d44fc5815d9376cfb7ecc13ce65ba
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-06 16:12:49 +0200

    update project build files

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Libs.gradleTools)
+        classpath(Libs.kotlinPlugin)
+        classpath(Libs.navigationSafeArgsPlugin)
+        classpath(Libs.googleServices)
+        classpath(Libs.hiltPlugin)
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+        google()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
