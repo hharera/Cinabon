1ef0713208bf8d63b96ebbf72e5e2234cd77ea13
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2022-09-29 08:00:37 +0200

    upgrade gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
         classpath(Libs.googleServices)
         classpath(Libs.hiltPlugin)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
 
